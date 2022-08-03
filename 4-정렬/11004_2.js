2f20aa61e20fd70a423eca6d015dcc1421a93907
luxetverit <replaysoccer@naver.com>
2022-08-03 17:53:49 +0900

    20220803

---
+++ 4-정렬/11004_2.js
@@ -0,0 +1,42 @@
+// 퀵정렬
+
+const fs = require('fs')
+const input = fs.readFileSync('ex.txt').toString().trim().split('\n')
+
+let [nk, arr] = input
+console.log(nk)
+nk = nk.split(' ').map(v => Number(v))
+arr = arr.split(' ').map(v => Number(v))
+
+const k = nk[1]
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left > right) {
+        return
+    }
+    const mid = Math.floor((left + right) / 2)
+    const pivot = arr[mid]
+    const partition = devide(arr, left, right, pivot)
+    quickSort(arr, left, partition - 1)
+    quickSort(arr, partition, right)
+
+    function devide(arr, left, right, pivot) {
+        while (left <= right) {
+            while (arr[left] < pivot) {
+                left++
+            }
+            while (arr[right] > pivot) {
+                right--
+            }
+            if (left <= right) {
+                let tmp = arr[left]
+                arr[left] = arr[right]
+                arr[right] = tmp
+                left++
+                right--
+            }
+        }
+        return left
+    }
+    return arr
+}
