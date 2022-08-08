157dc97503739a9aa7c3ea0cc718c24ee7db01d3
luxetverit <replaysoccer@naver.com>
2022-08-08 16:52:59 +0900

    20220808

--- 4-정렬/11004_2.js
+++ 4-정렬/11004_2.js
@@ -4,23 +4,25 @@ const fs = require('fs')
 const input = fs.readFileSync('ex.txt').toString().trim().split('\n')
 
 let [nk, arr] = input
-console.log(nk)
+//console.log(nk)
 nk = nk.split(' ').map(v => Number(v))
 arr = arr.split(' ').map(v => Number(v))
 
 const k = nk[1]
+let result = quickSort(arr)
+console.log(result[k - 1])
 
 function quickSort(arr, left = 0, right = arr.length - 1) {
-    if (left > right) {
+    if (left >= right) {
         return
     }
     const mid = Math.floor((left + right) / 2)
     const pivot = arr[mid]
-    const partition = devide(arr, left, right, pivot)
+    const partition = divide(arr, left, right, pivot)
     quickSort(arr, left, partition - 1)
     quickSort(arr, partition, right)
 
-    function devide(arr, left, right, pivot) {
+    function divide(arr, left, right, pivot) {
         while (left <= right) {
             while (arr[left] < pivot) {
                 left++
