de84464430d97a31498770089f78047d2a67d524
luxetverit <replaysoccer@naver.com>
2022-08-23 17:13:01 +0900

    20220823

---
+++ 6-구현/10430.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync('ex.txt').toString().trim().split(' ')
+
+const a = Number(input[0])
+const b = Number(input[1])
+const c = Number(input[2])
+let answer = ''
+
+console.log((a + b) % c)
+console.log(((a % c) + (b % c)) % c)
+console.log((a * b) % c)
+console.log(((a % c) * (b % c)) % c)
